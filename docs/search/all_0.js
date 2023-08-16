989bab6b0ff7a5ac503020aef961218489f65959
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-08-16 17:51:23 +0000

    Deploying to gh-pages from @ LTLA/CppWeightedLowess@5342e5af776b9b1eb2eebff6984f1fdd886ec2ac 🚀

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['anchors_0',['anchors',['../structWeightedLowess_1_1WeightedLowess_1_1Defaults.html#aa3cbce4611af4c89691e7fe04202598c',1,'WeightedLowess::WeightedLowess::Defaults']]]
+  ['anchors_0',['anchors',['../structWeightedLowess_1_1WeightedLowess_1_1Defaults.html#aba2444c6de1cf8357bc874b3955258eb',1,'WeightedLowess::WeightedLowess::Defaults']]]
 ];
--- docs/search/all_6.js
+++ docs/search/all_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['set_5fanchors_0',['set_anchors',['../classWeightedLowess_1_1WeightedLowess.html#aa7b6a6f3deb658900f3917f9542a0ceb',1,'WeightedLowess::WeightedLowess']]],
+  ['set_5fanchors_0',['set_anchors',['../classWeightedLowess_1_1WeightedLowess.html#a97bbe0f7e157d0418a0c40a142111eb2',1,'WeightedLowess::WeightedLowess']]],
   ['set_5fas_5ffrequency_5fweights_1',['set_as_frequency_weights',['../classWeightedLowess_1_1WeightedLowess.html#aa3047d5689dc33bea9e4deccad84913d',1,'WeightedLowess::WeightedLowess']]],
   ['set_5fdelta_2',['set_delta',['../classWeightedLowess_1_1WeightedLowess.html#aaa89b16768948436f15f3e4ada2b9098',1,'WeightedLowess::WeightedLowess']]],
   ['set_5fiterations_3',['set_iterations',['../classWeightedLowess_1_1WeightedLowess.html#ae762bb87d2d249691ffd90a971656f15',1,'WeightedLowess::WeightedLowess']]],
