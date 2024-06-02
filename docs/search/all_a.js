6f461fa46a6cf5e9210aec2ab8700428f78e226b
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-06-02 09:37:21 +0000

    Deploying to gh-pages from @ LTLA/CppWeightedLowess@0ee5b27199ce5954e13b8ac76dc87c26aa57c9c7 🚀

--- docs/search/all_a.js
+++ docs/search/all_a.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['sortby_0',['SortBy',['../classWeightedLowess_1_1SortBy.html',1,'WeightedLowess::SortBy'],['../classWeightedLowess_1_1SortBy.html#a33001f8b6442aa313a8f866c3f800196',1,'WeightedLowess::SortBy::SortBy()']]],
+  ['sortby_0',['sortby',['../classWeightedLowess_1_1SortBy.html',1,'WeightedLowess::SortBy'],['../classWeightedLowess_1_1SortBy.html#a33001f8b6442aa313a8f866c3f800196',1,'WeightedLowess::SortBy::SortBy()']]],
   ['sortby_2ehpp_1',['SortBy.hpp',['../SortBy_8hpp.html',1,'']]],
   ['span_2',['span',['../structWeightedLowess_1_1Options.html#a820614e4b0971b12560f420025876549',1,'WeightedLowess::Options']]],
   ['span_5fas_5fproportion_3',['span_as_proportion',['../structWeightedLowess_1_1Options.html#ab6408e30d42c2d6f328b1bc002c8e2d9',1,'WeightedLowess::Options']]]
