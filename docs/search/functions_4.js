880926ac1633a07e6391a289723d4928c58e6e30
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-06-25 15:53:37 +0000

    Deploying to gh-pages from @ LTLA/CppWeightedLowess@e4ed9a71859c4d602df899583c99560370b82b64 🚀

--- docs/search/functions_4.js
+++ docs/search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['sortby_0',['SortBy',['../classWeightedLowess_1_1SortBy.html#a33001f8b6442aa313a8f866c3f800196',1,'WeightedLowess::SortBy']]]
+  ['set_0',['set',['../classWeightedLowess_1_1SortBy.html#aff87b9087d04044a778f9a623f296d93',1,'WeightedLowess::SortBy']]],
+  ['sortby_1',['sortby',['../classWeightedLowess_1_1SortBy.html#a33001f8b6442aa313a8f866c3f800196',1,'WeightedLowess::SortBy::SortBy(size_t num_points, const Data_ *x)'],['../classWeightedLowess_1_1SortBy.html#ab86761ca01148dd783d9352271aa4d89',1,'WeightedLowess::SortBy::SortBy()=default']]]
 ];
